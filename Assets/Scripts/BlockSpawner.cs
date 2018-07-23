b1233b3baec41ef24cb89aac77327517d090accc
Charlie <league@iMac-2.attlocal.net>
2018-07-23 13:30:24 -0700

    Day1

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+    public GameObject BlockA;
+    public int counter = 0;
+    public float maxCount = 90;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (counter < maxCount)
+        {
+            counter++;
+        }
+        else
+        {
+            Instantiate(BlockA);
+            counter = 0;
+            maxCount = maxCount - 2f;
+        }
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Rigidbody2D rigBody;
+    public bool canJump = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey("w") & canJump == true){
+            canJump = false;
+            rigBody.AddForce(Vector3.up * 260);
+        }
+
+	}
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.tag == "Ground"){
+           canJump = true;
+        }
+	}
+}
